3abb75b2346ed998538d96d756aa4b58c7918f20
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-10-03 13:31:21 -0400

    move these classes up one directory

---
+++ dvpipe/pipelines/lmtmetadatablock.py
@@ -0,0 +1,52 @@
+from dvpipe.pipelines.lmtslr.metadatablock import MetadataBlock
+import pandas as d
+import json
+import dvpipe.utils as utils
+
+class LmtMetadataBlock(MetadataBlock):
+    def __init__(self):
+      self._datacsv = utils.aux_file("LMTMetaDatablock.csv")
+      self._vocabcsv =  utils.aux_file("LMTControlledVocabulary.csv")
+      super().__init__("LMTData",self._datacsv,self._vocabcsv)
+
+if __name__ == "__main__":
+    lmtdata = LmtMetadataBlock()
+    print(lmtdata.name,'\n',lmtdata.datasetFields)
+    print(lmtdata.datasetFields['name'].values)
+    lmtdata.add_metadata("projectID","2021-S1-US-3")
+    lmtdata.add_metadata("PIName","Marc Pound")
+    lmtdata.add_metadata("obsnum",12345)
+    lmtdata.add_metadata("RA",123.456)
+    lmtdata.add_metadata("DEC",-43.210)
+    lmtdata.add_metadata("slBand",1)
+    lmtdata.add_metadata("lineName",'CS2-1')
+    lmtdata.add_metadata("frequencyCenter",97.981)
+    lmtdata.add_metadata("bandwidth",2.5)
+    lmtdata.add_metadata("intTime",30.0)
+    lmtdata.add_metadata("projectTitle","Life, the Universe, and Everything")
+    lmtdata.add_metadata("obsDate",utils.now())
+    lmtdata.add_metadata("velocity",321.0)
+    lmtdata.add_metadata("velDef","RADIO")
+    lmtdata.add_metadata("velFrame","LSR")
+    lmtdata.add_metadata("velType","FREQUENCY")
+    lmtdata.add_metadata("z",0.001071)
+    lmtdata.add_metadata("beam",20.0/3600.0)
+    lmtdata.add_metadata("lineSens",0.072)
+    lmtdata.add_metadata("facility","LMT")
+    lmtdata.add_metadata("instrument","SEQUOIA")
+    lmtdata.add_metadata("object","NGC 5948")
+    try:
+        lmtdata.add_metadata("foobar",12345)
+    except KeyError as v:
+        print("Caught as expected: ",v)
+    print(lmtdata.controlledVocabulary)
+    print(lmtdata.check_controlled("velFrame","Foobar"))
+    print(lmtdata.check_controlled("velFrame","LSR"))
+    print(lmtdata.check_controlled("foobar","uhno"))
+    try:
+        lmtdata.add_metadata("velFrame","Foobar")
+    except ValueError as v:
+        print("Caught as expected: ",v)
+    print(lmtdata.to_yaml())
+    #print(lmtdata.datasetFields.to_json())
+
---
+++ dvpipe/pipelines/metadatablock.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import json
+import dvpipe.utils as utils
+class MetadataBlock(object):
+    '''Generic representation of a Dataverse metadata block.
+       Metadata blocks have a datasetField which gives the names and
+       properties of the metadata items, i.e. the metadata definition. 
+       Actual metadata conforming to this definition are in the 
+      'metadata' proprty.
+       Specific metadata blocks should inherit from this class.
+    '''
+    def __init__(self,name,dataset_file,vocabulary_file=None):
+        self._name = name 
+        self._dsfColnames = ['name', 'title', 'description', 'watermark',
+                          'fieldType', 'displayOrder', 'displayFormat', 
+                          'advancedSearchField', 'allowControlledVocabulary', 
+                          'allowmultiples', 'facetable', 'displayoncreate', 
+                          'required', 'parent', 'metadatablock_id']
+        self._cvColnames = ['DatasetField', 'Value', 
+                            'identifier', 'displayOrder']
+        # metdata definition
+        self._dataset_file = dataset_file
+        # controlled vocabulary definition
+        self._vocabulary_file = vocabulary_file
+        self._datasetFields = pd.read_csv(dataset_file)
+        if vocabulary_file is not None:
+            self._controlledVocabulary = pd.read_csv(vocabulary_file)
+        # The actual metadata
+        self._metadata = dict()
+    
+    @property
+    def name(self):
+        '''Name of this metadata block'''
+        return self._name
+
+    @property 
+    def datasetColnames(self):
+        return self._dsfColnames
+
+    @property
+    def datasetFields(self):
+        return self._datasetFields
+
+    @property 
+    def controlledVocabularyColnames(self):
+        return self._cvColnames
+
+    @property
+    def controlledVocabulary(self):
+        return self._controlledVocabulary
+
+    @property 
+    def metadata(self):
+        return self._metadata
+ 
+    # NB a dict will not allow repeated entries (allowmultiples=True)
+    # switch to DataFrame?
+    def add_metadata(self,name,value):
+        if name not in self._datasetFields['name'].values:
+            raise KeyError(f'{name} is not a recognized dataset field in {self.name}')
+        #@todo check value against controlled vocabulary
+        if not self.check_controlled(name,value):
+            s =  self._allowed_values(name,value)
+            raise ValueError(f'{value} is not a valid value for dataset field {name} in {self.name}. Allowed values are: {s}.')
+        self._metadata[name] = value
+
+    def _allowed_values(self,name,value):
+        '''The allowed values of a variable if in a controlled vocabulary 
+           (i.e., the enums).  Will return empty list if the variable 
+           is not controlled
+        '''
+        series =  self._controlledVocabulary.loc[self._controlledVocabulary["DatasetField"] == name]["Value"]
+        return series.values
+
+    def check_controlled(self,name,value):
+        s =  self._allowed_values(name,value)
+        return s.size == 0 or value in s
+
+    def to_yaml(self,indent=4):
+        js = json.dumps(self._metadata,indent=indent)
+        return utils.pformat_yaml(js)
