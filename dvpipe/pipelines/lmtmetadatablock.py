a83effaa1de3d086539e5fccbb162101471a4674
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-10-21 14:39:19 -0400

    allow parent/child relationships

--- dvpipe/pipelines/lmtmetadatablock.py
+++ dvpipe/pipelines/lmtmetadatablock.py
@@ -16,28 +16,44 @@ if __name__ == "__main__":
     print("LMTMetadata version",lmtdata.version)
     print(lmtdata.datasetFields['name'].values)
     lmtdata.add_metadata("projectID","2021-S1-US-3")
+    lmtdata.add_metadata("projectTitle","Life, the Universe, and Everything")
     lmtdata.add_metadata("PIName","Marc Pound")
     lmtdata.add_metadata("obsnum","12345") 
 #   for multiple obsnums:
     lmtdata.add_metadata("obsnum","12345,56783,42099") 
     lmtdata.add_metadata("RA",123.456)
     lmtdata.add_metadata("DEC",-43.210)
-    lmtdata.add_metadata("slBand",1)
-    lmtdata.add_metadata("lineName",'CS2-1')
+    band = dict()
+    band["slBand"] = 1
+    band["lineName"]='CS2-1'
 #   for multiple lines:
-    #lmtdata.add_metadata("lineName",'CS2-1,CO1-0,H2CS')
-    lmtdata.add_metadata("frequencyCenter",97.981)
-    lmtdata.add_metadata("bandwidth",2.5)
-    lmtdata.add_metadata("intTime",30.0)
-    lmtdata.add_metadata("projectTitle","Life, the Universe, and Everything")
+    #band["lineName"] = 'CS2-1,CO1-0,H2CS'
+    band["frequencyCenter"] = 97.981
+    band["bandwidth"] = 2.5
+    band["beam"] = 20.0/3600.0
+    band["lineSens"] = 0.072
+    band["qaGrade"] = "A+++"
+    lmtdata.add_metadata("band",band)
+
+    # add a second band
+    band["slBand"] = 2
+    band["lineName"]='CO1-0'
+#   for multiple lines:
+    #band["lineName"] = 'CS2-1,CO1-0,H2CS'
+    band["frequencyCenter"] = 115.2712
+    band["bandwidth"] = 2.5
+    band["beam"] = (97.981/115.2712)*20.0/3600.0
+    band["lineSens"] = 0.1
+    band["qaGrade"] = "B-"
+    lmtdata.add_metadata("band",band)
+
     lmtdata.add_metadata("obsDate",utils.now())
+    lmtdata.add_metadata("intTime",30.0)
     lmtdata.add_metadata("velocity",321.0)
     lmtdata.add_metadata("velDef","RADIO")
     lmtdata.add_metadata("velFrame","LSR")
     lmtdata.add_metadata("velType","FREQUENCY")
     lmtdata.add_metadata("z",0.001071)
-    lmtdata.add_metadata("beam",20.0/3600.0)
-    lmtdata.add_metadata("lineSens",0.072)
     lmtdata.add_metadata("observatory","LMT")
     lmtdata.add_metadata("LMTInstrument","SEQUOIA")
     lmtdata.add_metadata("targetName","NGC 5948")
@@ -46,13 +62,22 @@ if __name__ == "__main__":
     except KeyError as v:
         print("Caught as expected: ",v)
     print(lmtdata.controlledVocabulary)
-    print(lmtdata.check_controlled("velFrame","Foobar"))
-    print(lmtdata.check_controlled("velFrame","LSR"))
-    print(lmtdata.check_controlled("foobar","uhno"))
+    print(lmtdata._check_controlled("velFrame","Foobar"))
+    print(lmtdata._check_controlled("velFrame","LSR"))
+    print(lmtdata._check_controlled("foobar","uhno"))
     try:
         lmtdata.add_metadata("velFrame","Foobar")
     except ValueError as v:
         print("Caught as expected: ",v)
-    print(lmtdata.to_yaml())
-    #print(lmtdata.datasetFields.to_json())
+    print(lmtdata._has_parent("slBand"))
+    print(lmtdata._has_parent("velFrame"))
+    print(lmtdata._is_parent("band"))
+    print(lmtdata.get_children("band"))
+    print(lmtdata.get_children("targetName"))
 
+    print("YAML")
+    print(lmtdata.to_yaml())
+    print("JSON")
+    print(lmtdata.to_json())
+
+    
--- dvpipe/pipelines/metadatablock.py
+++ dvpipe/pipelines/metadatablock.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import json
 import dvpipe.utils as utils
+from copy import deepcopy
+
 class MetadataBlock(object):
     '''Generic representation of a Dataverse metadata block.
        Metadata blocks have a datasetField which gives the names and
@@ -23,6 +25,7 @@ class MetadataBlock(object):
         # controlled vocabulary definition
         self._vocabulary_file = vocabulary_file
         self._datasetFields = pd.read_csv(dataset_file)
+        self._datasetFields.set_index("parent")
         if vocabulary_file is not None:
             self._controlledVocabulary = pd.read_csv(vocabulary_file)
         # The actual metadata
@@ -62,10 +65,24 @@ class MetadataBlock(object):
         if name not in self._datasetFields['name'].values:
             raise KeyError(f'{name} is not a recognized dataset field in {self.name}')
         # check value against controlled vocabulary
-        if not self.check_controlled(name,value):
+        if not self._check_controlled(name,value):
             s =  self._allowed_values(name,value)
             raise ValueError(f'{value} is not a valid value for dataset field {name} in {self.name}. Allowed values are: {s}.')
-        self._metadata[name] = value
+        isparent = self._is_parent(name)
+        if isparent  and type(value) is not dict:
+            raise ValueError(f'Dataset field {self.name} has children whose values must be passed as a dict: {self.get_children(name)}')
+        if self._has_parent(name):
+            parent = self.get_parent(name)
+            raise ValueError(f'Dataset field "{name}" is a child of "{parent}" and passed as a dict member for "{parent}". e.g. add_metadata({parent},{{"{name}":...}}')
+        if isparent:
+            # Note: must use deepcopy of dict because caller may reuse the
+            # the dict in calling object and this is normally just a reference.
+            if name in self._metadata:
+                self._metadata[name].append(deepcopy(value))
+            else:
+                self._metadata[name]= [deepcopy(value)]
+        else:
+            self._metadata[name] = value
 
     def _allowed_values(self,name,value):
         '''The allowed values of a variable if in a controlled vocabulary 
@@ -75,11 +92,37 @@ class MetadataBlock(object):
         series =  self._controlledVocabulary.loc[self._controlledVocabulary["DatasetField"] == name]["Value"]
         return series.values
 
-    def check_controlled(self,name,value):
+    def _check_controlled(self,name,value):
         s =  self._allowed_values(name,value)
         return s.size == 0 or value in s
 
-    def to_yaml(self,indent=4):
-        js = json.dumps(self._metadata,indent=indent)
+    def _has_parent(self,name):
+        df = self._datasetFields[self._datasetFields['name'] == name]
+        return df.notna()["parent"].values[0]
+
+    def _is_parent(self,name):
+        df = self._datasetFields[self._datasetFields['name'] == name]
+        return df["fieldType"].values[0] == "none"
+
+    def get_parent(self,name):
+        df = self._datasetFields[self._datasetFields['name'] == name]
+        return df["parent"].values[0]
+
+    def get_children(self,name):
+        '''Identify the children of the given dataset field
+        
+        :param name: the parent field to check for children
+        :type name: str
+        :return: list of names of children. This list will be empty if the input field has no children
+        :rtype: numpy.ndarray
+        '''
+        df = self._datasetFields[self._datasetFields['parent'] == name]
+        return df['name'].values
+
+    def to_yaml(self):
+        comment = f"# {self.name} metadata block version {self.version}"
+        return comment+utils.pformat_yaml(self._metadata)
+
+    def to_json(self,indent=4):
         comment = f"# {self.name} metadata block version {self.version}\n"
-        return utils.pformat_yaml(comment+js)
+        return comment+json.dumps(self._metadata,indent=indent)
