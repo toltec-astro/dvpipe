9c5a9a6ab7c704f369f7cbb230be2866efd9086f
Marc Pound <22331890+mpound@users.noreply.github.com>
2023-04-05 11:13:09 -0400

    remove example so peter is not confused

--- dvpipe/pipelines/lmtmetadatablock.py
+++ dvpipe/pipelines/lmtmetadatablock.py
@@ -117,60 +117,6 @@ class LmtMetadataBlock(MetadataBlock):
             print("JSON")
             print(self.to_json())
 
-def example(dbfile=None,yamlfile=None):
-    '''Example usage of LmtMetadataBlock'''
-    lmtdata = LmtMetadataBlock(dbfile=dbfile,yamlfile=yamlfile)
-    lmtdata.add_metadata("projectID","2021-S1-US-3")
-    lmtdata.add_metadata("projectTitle","Life, the Universe, and Everything")
-    lmtdata.add_metadata("PIName","Marc Pound")
-    lmtdata.add_metadata("obsnum","12345") 
-#   for multiple obsnums:
-    #lmtdata.add_metadata("obsnum","12345,56783,42099") 
-    lmtdata.add_metadata("RA",14.01,"degree")
-    lmtdata.add_metadata("DEC",-43.210)
-    # add a band
-    band = dict()
-    band["slBand"] = 1
-    band["formula"]='CS'
-    band["transition"]='2-1'
-    band["frequencyCenter"] = 97981*u.Unit("MHz")
-    band["velocityCenter"] = 300.0 #km/s
-    band["bandwidth"] = 2.5
-    band["beam"] = u.Quantity(20.0,"arcsec")
-    band["lineSens"] = 0.072
-    band["qaGrade"] = "A+++"
-    band["nchan"] = 1024
-    lmtdata.add_metadata("band",band)
-    # add a second band
-    band["slBand"] = 2
-    band["formula"]='CO'
-    band["transition"]='1-0'
-#   for multiple lines:
-    band["frequencyCenter"] = u.Quantity(115.2712,"GHz")
-    band["bandwidth"] = 2.5 #GHz
-    band["beam"] = (97.981/115.2712)*20.0/3600.0
-    band["lineSens"] = 123*u.Unit("mK")
-    band["qaGrade"] = "B-"
-    band["velocityCenter"] = -25.0 #km/s
-    band["nchan"] = 2048
-    lmtdata.add_metadata("band",band)
-
-    lmtdata.add_metadata("obsDate",utils.now())
-    lmtdata.add_metadata("intTime",30.0,"minute")
-    lmtdata.add_metadata("velocity",321.0,u.Unit("m/s"))
-    lmtdata.add_metadata("velDef","RADIO")
-    lmtdata.add_metadata("velFrame","LSR")
-    lmtdata.add_metadata("velType","FREQUENCY")
-    lmtdata.add_metadata("z",0.001071)
-    lmtdata.add_metadata("observatory","LMT")
-    lmtdata.add_metadata("LMTInstrument","SEQUOIA")
-    lmtdata.add_metadata("targetName","NGC 5948")
-    lmtdata.add_metadata("calibrationStatus","UNCALIBRATED")# or CALIBRATED
-    # YAML output
-    #print(lmtdata.to_yaml())
-    return lmtdata
-
-
 class CitationMetadataBlock(MetadataBlock):
     def __init__(self):
       self._datacsv = utils.aux_file("CitationMetaDatablock.csv")
